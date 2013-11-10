2e4061f5c2d7db898d7f22e5043920c93a6da0e9
Ashot Galstyan <moypriloveok@gmail.com>
2013-11-10 18:14:10 +0300

    fix email

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 	<footer>
 		<div class="row">
 			<div class="col-lg-12">
-				<p>email: <a href="mailto:info@karinagalstyan.by" target="_top">info@karinagalstyan.by</a></p>
+				<p>email: <a href="mailto:info@karinagalstyan.by" target="_top">karina_galstian@mail.ru</a></p>
 				<p>&copy; 2013 Karina Galstyan</p>
 			</div>
 		</div>
